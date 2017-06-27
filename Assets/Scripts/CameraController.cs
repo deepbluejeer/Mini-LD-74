14e78519f9d3151da52fc5f2cc7bb301c37fd74c
deepbluejeer <xellotath1@gmail.com>
2017-06-27 15:02:44 -0300

    Início do projeto

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+
+    PlayerController player;
+
+    bool isFollowing;
+
+    public float xOffset;
+    public float yOffset;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        isFollowing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFollowing)
+            transform.position = new Vector3(player.transform.position.x + xOffset,
+                                              yOffset,
+                                              player.transform.position.z - 1);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float jumpHeight;
+	public float speed;
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
+	private Rigidbody2D rgb;
+
+	void Start ()
+	{
+		rgb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate ()
+	{
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+	void Update ()
+	{
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetButtonDown("Jump") && grounded)
+            rgb.velocity = new Vector2(0, jumpHeight);
+
+        rgb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rgb.velocity.y);
+    }
+}
